620b20b2c648f0e300f4a6a7ae3e1704fef98cbd
Montii <llh9@yahoo.com>
2022-03-22 13:21:16 -0400

    some server files added

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
+import GoogleLogin from 'react-google-login';
+
 import './App.css';
 
 function App() {
+
+  const responseSuccessGoogle = (response) => {
+    console.log(response);
+  }
+
+  const responseErrorGoogle = (response) => {
+    console.log(response);
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <GoogleLogin
+          clientId="30907382547-824taveus218anrvdmomvsg046o1v57i.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={responseSuccessGoogle}
+          onFailure={responseErrorGoogle}
+          cookiePolicy={'single_host_origin'}
+        />,
+        document.getElementById('googleButton')
+
       </header>
-    </div>
+      </div>
   );
 }
 
